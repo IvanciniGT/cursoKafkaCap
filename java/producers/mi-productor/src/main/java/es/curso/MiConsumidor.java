942b19d6ec8ef0cf5435026a6b39a002f29bf0a2
Ubuntu <ubuntu@ip-172-31-7-114.eu-west-1.compute.internal>
2021-06-08 18:10:31 +0000

    ioexception

--- java/producers/mi-productor/src/main/java/es/curso/MiConsumidor.java
+++ java/producers/mi-productor/src/main/java/es/curso/MiConsumidor.java
@@ -9,6 +9,7 @@ import org.apache.kafka.clients.consumer.ConsumerRecords ;
 import java.util.Properties;
 import java.util.Arrays;
 import java.time.Duration;
+import java.io.IOException;
 
 public class MiConsumidor  {
     
